463c1a5974dc29c8f02a08504ff353309ce474e3
roni9843 <ronironi98439843@gmail.com>
2024-01-13 20:01:44 +0600

    update

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,4 @@
 import io from "socket.io-client";
-const socket = io.connect("http://localhost:8080/");
+const socket = io.connect("https://garments-backend.onrender.com/");
+// const socket = io.connect("http://localhost:8080/");
 export default socket;
